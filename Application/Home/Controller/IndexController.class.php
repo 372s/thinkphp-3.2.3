dabe358c86a848bc4984f281d89950a547863f2f
wq455133 <wq455133@163.com>
2018-12-04 13:39:06 +0800

    update

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,14 +3,27 @@ namespace Home\Controller;
 
 use Think\Log;
 use Think\Crypt;
+use Think\Image;
 
 Vendor('phpQuery.phpQuery');
 
 class IndexController extends BaseController {
 
+    public function add() {
+
+    }
     public function index(){
         // TODO: Common 目录怎么使用
         // echo dpe();die;
+
+        $h = file_get_contents('https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9844321684069543017%22%2C%22sourceFrom%22%3A%22bjh%22%7D&type=news');
+        echo $h;die;
+        $img = "https://f12.baidu.com/it/?u=2674946297,2122048651&fm=173&app=49&f=JPEG?w=640&h=480&s=D77816C50C72ECD65A3FF8240300B058&access=215967316";
+        $res = file_get_contents($img);
+        var_dump($res);die;
+        $im = new Image(2, $img);
+        print_r($im);die;
+
         // 测试LOG
         Log::record('ID:5461313213', 'DEBUG');
         print_r($this->keywords);die;
