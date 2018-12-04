f5ccca233bb34dd2ec218195116a43cf01ed7d02
wq455133 <wq455133@163.com>
2018-12-05 01:40:12 +0800

    update

---
+++ Application/Home/Controller/HomeController.class.php
@@ -0,0 +1,79 @@
+<?php
+namespace Home\Controller;
+
+
+class HomeController extends BaseController
+{
+    public function index() {
+        $content = '<meta charset="utf-8">';
+        $content .= "<div><p><span><span>导语：</span>中国汽车市场有个特殊现象，一些外资品牌从海外市场原汁原味引进的车型，反而容易遭遇“滑铁卢”。主要原因是海外市场的车型，不一定适合中国消费者的口味，而在满足中国汽车用户的需求方面，自主品牌更有优势。</span></p><p><span>我们有句成语叫做“闭门造车”，比喻固步自封，脱离实际只凭主观办事。这句从造车引申而来的成语，用于当下的汽车市场非常贴切。随着汽车市场竞争的加剧，消费者的口味也愈加“刁钻”，不考虑市场和用户需求，无法满足消费升级，汽车作为一种大件商品自然也无法获得消费者青睐。虽然不少车企在新品上市时会宣称坚持用户导向的造车理念，但真正做到了并且做得好的并不多。作为SUV的领导品牌，哈弗旗下的F系旗舰车型哈弗F7做到了。</span></p><div><div><div><img src=\"https://f12.baidu.com/it/u=235744853,3465274524&amp;fm=173&amp;app=49&amp;f=JPEG?w=640&amp;h=409&amp;s=44A02BF14D1214DA484D69870300E0C2&amp;access=215967316\"></div></div></div><p><span>驾乘舒适度高，车舱环境健康环保</span></p><p><span>从买车的需求看，很多购买SUV的消费者都是为了全家出行，家庭成员乘坐舒适是购车的一项重要刚需，而且中国家庭人口多，后排座椅利用率高。乘坐舒适这个需求看似简单，但要做好并没有这么简单，不仅前后排空间要宽敞，还要求座椅舒适性好并符合人体工程学，行驶中NVH表现好，内饰环保健康。</span></p><div><div><div><img src=\"https://f10.baidu.com/it/u=3716921928,3797460825&amp;fm=173&amp;app=49&amp;f=JPEG?w=640&amp;h=407&amp;s=1E32FFA00C12B6CC120780920300E0C3&amp;access=215967316\"></div></div></div><p><span>哈弗F7的乘坐空间非常宽裕，其轴距达到2725mm，在同级产品中优势较大，甚至接近部分中型SUV，而且车长和车宽也是同级主流，加上对后排乘坐空间的特别优化，后排乘客的头部和腿部空间已经可以比拟中型SUV，哈弗F7的后排地板中间也接近全平，后排乘坐三个人毫无压力。后排座椅靠背可以调节角度，能调到半躺的姿势。前排座椅为6向电动调节，不同体型的驾驶员都能找到舒适的坐姿。哈弗F7的座椅填充物也相当厚实，进一步提升了舒适度。</span></p><div><div><div><img src=\"https://f12.baidu.com/it/u=2581145636,1177815491&amp;fm=173&amp;app=49&amp;f=JPEG?w=640&amp;h=428&amp;s=6C9243962E0302CC048DF5920300D093&amp;access=215967316\"></div></div></div><p><span>哈弗F7在NVH方面也进行了特别优化，在法定最高速度120km/h行驶时也不影响车内交流，乘坐静谧性良好。</span></p><div><div><div><img src=\"https://f12.baidu.com/it/u=1612063481,1441745698&amp;fm=173&amp;app=49&amp;f=JPEG?w=640&amp;h=427&amp;s=2FB06884D3AA3CA67AED15160300C0D1&amp;access=215967316\"></div></div></div><p><span>另外，越来越多年轻消费者关注内饰的环保性，希望能有更健康的车内环境，近年也有不少车型因为内饰异味大、内饰材质环保不达标等原因导致大批用户投诉。哈弗F7充分考虑了消费者的健康需求，内饰采用成本更高的环保材料和专属的香氛系统，车内甲醛是国标的1/5，甲苯、乙苯含量是国标的1/100，配合PM2.5过滤功能和香氛系统，让你在雾霾天出行也不怕。</span></p><p><span>智能互联技术给用车带来更多便利</span></p><p><span>年轻消费者们已经习惯了移动互联网和智能手机带来的便利，也希望车辆能时刻保持互联，并实现导航、收听在线电台、语音控制等功能。哈弗F7在这方面达到了更高的标准，被称为最聪明、最懂你的SUV。</span></p><div><div><div><img src=\"https://f12.baidu.com/it/u=608361098,2949685214&amp;fm=173&amp;app=49&amp;f=JPEG?w=640&amp;h=360&amp;s=3EA669845FEB0D0D22FDD116030050C1&amp;access=215967316\"></div></div></div><p><span>哈弗F7配备了同级最领先的智能交互系统，具备一定的AI属性。除了常规的车机互联、APP远程控制之外，哈弗F7还能实现人和车一定程度的交流，该系统能识别自然语言而不是一些固定的语音指令，并涵盖“衣食住行用”多个方面，语音识别率高达97%。</span></p><p><span>如果是普通车载系统，想要导航到某个餐厅或者调节空调温度，需要用手在导航系统内输入地址和进行点击，空调也需要调节按钮，不仅步骤繁琐而且影响行车安全。即使支持语音输入，多数只能识别某些特定的指令并需要多次确认。而哈弗F7的智能交互系统，你只需要说“我饿了”，系统会推荐附近的餐厅；说“我冷了”，系统自动把空调调高。给用车提供很多乐趣和便利。</span></p><div><div><div><img src=\"https://f12.baidu.com/it/u=3959580191,3646086993&amp;fm=173&amp;app=49&amp;f=JPEG?w=640&amp;h=480&amp;s=85C3FB044789034D4AD421990100C082&amp;access=215967316\"></div></div></div><p><span>此外，哈弗F7的智能娱乐系统提供2000万+在线QQ音乐、考拉FM电台节目等海量内容，可以将手机的收听习惯移植到车内，而且4G流量三年免费无限量使用。 </span></p><p><span>越级配置打造越级安全</span></p><p><span>美国心理学家马斯洛的需要层次理论指出，人的需要可分为五个层次，其中安全需要位于第二个层次。安全性能也已经成为购车的一个重要指标，消费者都希望能买到一辆最安全的汽车。哈弗一直将安全性能作为一个品牌特色，哈弗F7也不例外。在被动安全方面，哈弗F7全车高强度钢比例超过65%，热成型钢及超高强度钢比例高达33.4%，为同级车型最高。A柱、B柱等关键部位甚至采用了德国进口钢材。</span></p><div><div><div><img src=\"https://f11.baidu.com/it/u=2471118148,4120583106&amp;fm=173&amp;app=49&amp;f=JPEG?w=640&amp;h=325&amp;s=0FB5EC0008010AC69681D5030100E0C1&amp;access=215967316\"></div></div></div><p><span>除了常规的ESP系统、胎压监测之外，智能安全配置是哈弗F7的另一个特色。哈弗F7提供了AEB自动刹车系统，并带有行人识别功能，在驾驶员不小心走神没有注意到前车或行人等意外情况下，系统将主动采取制动减轻或避免碰撞，2014年开始欧洲E-NCAP测试要求配备AEB系统是车辆获得五星评价的必备条件，现在该系统已在哈弗F7上配备。多数同级车型的AEB系统只能识别车辆，哈弗F7还能识别行人是个很大亮点，这也是哈弗F7“以人为本”造车理念的一个体现。</span></p><div><div><div><img src=\"https://f11.baidu.com/it/u=2855877088,2751176945&amp;fm=173&amp;app=49&amp;f=JPEG?w=640&amp;h=279&amp;s=38C5A14C5FA8AB62045E15140300C0C8&amp;access=215967316\"></div></div></div><p><span>此外，LCA并线辅助、LDW车道偏离预警等常规功能一应俱全，TSR交通限速标志智能识别、DOW开门预警则是同级独有，这些智能安全配置可以一定程度上可以预防事故发生，提升主动安全性，让车主和乘客更有安全感。</span></p><p><span>小结</span></p><p><span>人体正常体温是36.3-37度，一旦遇到心动的人，就会心跳加速，体温微微上升到37.2度，感觉到贴心和温暖。哈弗F7正是这样一辆能让消费者时刻感觉温暖的车型，全方位满足舒适、安全的用车刚需。从乘坐舒适度、智能互联技术和安全性能来看，哈弗F7的37.2度科技正符合年轻消费者的喜好，再加上高颜值的外观和高规格的动力系统，哈弗F7很有可能成为哈弗旗下又一“神车”。</span></p><p><span></span></p></div>";
+        $dom = new \DOMDocument('1.1', 'utf-8');
+        @$dom->loadHTML($content);
+        //Evaluate Anchor tag in HTML
+        $xpath = new \DOMXPath($dom);
+        $hrefs = $xpath->evaluate("//img");
+
+        $arr_src = array();
+        $new_src = array();
+
+        if ($hrefs->length == 0) {
+            echo $content;die;
+        }
+
+        foreach ($hrefs as $value) {
+            $imgsrc = $value->getAttribute('src');
+            $host = 'mbd.baidu.com';
+            $result = $this->curl_get_content($imgsrc, $host);
+
+            $img_path = dirname(__DIR__) . '/uploads/';
+            $filename = $img_path.md5($imgsrc);
+            file_put_contents($filename, $result);
+            $xinarc = create_img($filename, $img_path);
+
+            $xinarc = str_replace(dirname(__DIR__), "http://php-study.test", $xinarc);
+            $new_src[] = $xinarc;
+            $value->setAttribute('src', $xinarc);
+        }
+        $content = $dom->saveHTML();
+        preg_match('/<body>(.*)<\/body>/is', $content, $ma);
+
+        print_r($ma[1]);die;
+        echo $content;die;
+    }
+
+    public function curl_get_content($url, $referer) {
+        $curl = curl_init();
+        $setopt = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_REFERER => $referer,
+            // CURLOPT_HTTPHEADER => array(
+            //     "Accept: */*",
+            //     "Accept-Encoding: gzip, deflate, br",
+            //     "Accept-Language: zh-CN,en-US;q=0.7,en;q=0.3",
+            //     "Cache-Control: max-age=0",
+            //     "Connection: keep-alive",
+            //     "cookie: BAIDUID=8667E5CD725AF880B79D96213972AF64:FG=1; BIDUPSID=8667E5CD725AF880B79D96213972AF64; PSTM=1542769852; H_PS_PSSID=1421_21106_27509; delPer=0; PSINO=1; BDRCVFR[gltLrB7qNCt]=mk3SLVN4HKm",
+            //     "DNT: 1",
+            //     "Host: f10.baidu.com",
+            //     "Referer: https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9844321684069543017%22%2C%22sourceFrom%22%3A%22bjh%22%7D&type=news",
+            //     "TE：Trailers",
+            //     "User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36",
+            // ),
+        );
+        curl_setopt_array($curl, $setopt);
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($response === false) {
+            return false;
+        } else {
+            return $response;
+        }
+    }
+}
